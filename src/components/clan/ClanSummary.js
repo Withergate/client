611f0d9cca1c885a33eac69f9f7344990a3a90f2
Martin Myslik <m.myslik@gmail.com>
2018-09-19 14:17:01 +0200

    task/#21 Add icons for income to notifications

--- src/components/clan/ClanSummary.js
+++ src/components/clan/ClanSummary.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import junk from '../../images/junk.png';
+import caps from '../../images/caps.png';
+import fame from '../../images/fame.png';
+
 const ClanSummary = ({clan}) => (
     <div className="m-4 p-2 rounded bg-light">
         <div className="row">
@@ -9,11 +13,14 @@ const ClanSummary = ({clan}) => (
             </div>
         </div>
         <div className="row">
-            <div className="col col-lg-2">
-                <b>Caps</b>: {clan.caps}
+            <div className="col-12 col-md-2">
+                <img height="20" src={caps} alt="caps" /> <b>Caps</b>: {clan.caps}
             </div>
-            <div className="col col-lg-2">
-                <b>Junk</b>: {clan.junk}
+            <div className="col-12 col-md-2">
+                <img height="20" src={junk} alt="junk" /> <b>Junk</b>: {clan.junk}
+            </div>
+            <div className="col-12 col-md-2">
+                <img height="20" src={fame} alt="fame" /> <b>Fame</b>: {clan.fame}
             </div>
         </div> 
     </div>
--- src/components/notification/NotificationListItem.js
+++ src/components/notification/NotificationListItem.js
@@ -2,17 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import renderHTML from 'react-render-html';
 
+import junk from '../../images/junk.png';
+import caps from '../../images/caps.png';
+import fame from '../../images/fame.png';
+import injury from '../../images/injury.png';
+import healing from '../../images/healing.png';
+import item from '../../images/item.png';
+import person from '../../images/person.png';
+
 const NotificationListItem = ({notification}) => (
     <div className="m-4 p-2 rounded bg-light">
+        <div>
+            { notification.junkIncome > 0 && <span><b>{notification.junkIncome}</b> <img height="20" src={junk} alt="junk" /> </span> }
+            { notification.capsIncome > 0 && <span><b>{notification.capsIncome}</b> <img height="20" src={caps} alt="caps" /> </span> }
+            { notification.fameIncome > 0 && <span><b>{notification.fameIncome}</b> <img height="20" src={fame} alt="fame" /> </span> }
+            { notification.injury > 0 && <span><b>{notification.injury}</b> <img height="20" src={injury} alt="injury" /> </span> }
+            { notification.healing > 0 && <span><b>{notification.healing}</b> <img height="20" src={healing} alt="healing" /> </span> }
+            { notification.itemIncome !== null && <span> <img height="20" src={item} alt="item" /> </span> }
+            { notification.characterIncome !== null && <span> <img height="20" src={person} alt="character" /> </span> }
+        </div>
         <div className="row p-3">
+            
             {renderHTML(notification.text)}
             &nbsp;
             {notification.result !== null && renderHTML(notification.result)}
         </div>
         {
-            notification.income !== null && 
+            notification.itemIncome !== null && 
             <div className="row p-3">
-                <small>{renderHTML(notification.income)}</small>
+                <small>{renderHTML(notification.itemIncome)}</small>
             </div>
         }
         {
