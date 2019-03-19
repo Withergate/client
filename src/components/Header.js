ee5c5e497a356ad0f5dec733152a3520f6ba357e
Martin Myslik <m.myslik@gmail.com>
2019-03-19 10:56:26 +0100

    task/#33 Add refresh button

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import logo from '../images/logo.png';
+import refresh from '../images/refresh.png';
 
 const Header = ({turn, userRole}) => (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -20,6 +21,9 @@ const Header = ({turn, userRole}) => (
             </ul>
         </div>
         <div className="text-light"><small><b>Turn: </b>{turn.turnId}</small></div>
+        <div className="ml-2" value="Refresh Page" onClick={() => window.location.reload(true)}>
+            <img height="20" src={refresh} alt="Refresh" />
+        </div>
     </nav>
 );
 
--- src/services/constants/endpoints.js
+++ src/services/constants/endpoints.js
@@ -1,15 +1,15 @@
 export const AUTH_URL = 'http://auth.withergate.com/';
-// export const API_URL = 'http://api.withergate.com/';
-export const API_URL = 'http://localhost:8080/';
+export const API_URL = 'http://api.withergate.com/';
+// export const API_URL = 'http://localhost:8080/';
+
+export const TOKEN_URL =
+  AUTH_URL +
+  'oauth/authorize' +
+  '?response_type=token&client_id=withergate&redirect_uri=http://www.withergate.com';
 
 /*
 export const TOKEN_URL =
   AUTH_URL +
   'oauth/authorize' +
-  '?response_type=token&client_id=withergate&redirect_uri=http://www.withergate.com';
-*/
-
-export const TOKEN_URL =
-  AUTH_URL +
-  'oauth/authorize' +
   '?response_type=token&client_id=withergate&redirect_uri=http://localhost:3000';
+*/
