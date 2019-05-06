0f6894c72fda46f310685b64543f597c13ab837c
Martin Myslik <martin.myslik@ataccama.com>
2019-05-06 16:02:59 +0200

    task/#74 Consolidate character actions into single reducer action

--- src/actions/actionActions.js
+++ src/actions/actionActions.js
@@ -20,45 +20,10 @@ import { FETCH_CLAN } from './dataActions';
 
 export const CREATE_CLAN = 'CREATE_CLAN';
 
-export const EQUIP_ITEM = 'EQUIP_ITEM';
-export const EQUIP_ITEM_PENDING = 'EQUIP_ITEM_PENDING';
-export const EQUIP_ITEM_FULFILLED = 'EQUIP_ITEM_FULFILLED';
-export const EQUIP_ITEM_REJECTED = 'EQUIP_ITEM_REJECTED';
-
-export const UNEQUIP_ITEM = 'UNEQUIP_ITEM';
-export const UNEQUIP_ITEM_PENDING = 'UNEQUIP_ITEM_PENDING';
-export const UNEQUIP_ITEM_FULFILLED = 'UNEQUIP_ITEM_FULFILLED';
-export const UNEQUIP_ITEM_REJECTED = 'UNEQUIP_ITEM_REJECTED';
-
-export const USE_CONSUMABLE = 'USE_CONSUMABLE';
-export const USE_CONSUMABLE_PENDING = 'USE_CONSUMABLE_PENDING';
-export const USE_CONSUMABLE_FULFILLED = 'USE_CONSUMABLE_FULFILLED';
-export const USE_CONSUMABLE_REJECTED = 'USE_CONSUMABLE_REJECTED';
-
-export const BUILDING = 'BUILDING';
-export const BUILDING_PENDING = 'BUILDING_PENDING';
-export const BUILDING_FULFILLED = 'BUILDING_FULFILLED';
-export const BUILDING_REJECTED = 'BUILDING_REJECTED';
-
-export const QUEST = 'QUEST';
-export const QUEST_PENDING = 'QUEST_PENDING';
-export const QUEST_FULFILLED = 'QUEST_FULFILLED';
-export const QUEST_REJECTED = 'QUEST_REJECTED';
-
-export const TRADE_RESOURCES = 'TRADE_RESOURCES';
-export const TRADE_RESOURCES_PENDING = 'TRADE_RESOURCES_PENDING';
-export const TRADE_RESOURCES_FULFILLED = 'TRADE_RESOURCES_FULFILLED';
-export const TRADE_RESOURCES_REJECTED = 'TRADE_RESOURCES_REJECTED';
-
-export const VISIT_LOCATION = 'VISIT_LOCATIONS';
-export const VISIT_LOCATION_PENDING = 'VISIT_LOCATIONS_PENDING';
-export const VISIT_LOCATION_FULFILLED = 'VISIT_LOCATIONS_FULFILLED';
-export const VISIT_LOCATION_REJECTED = 'VISIT_LOCATIONS_REJECTED';
-
-export const REST_CHARACTER = 'REST_CHARACTER';
-export const REST_CHARACTER_PENDING = 'REST_CHARACTER_PENDING';
-export const REST_CHARACTER_FULFILLED = 'REST_CHARACTER_FULFILLED';
-export const REST_CHARACTER_REJECTED = 'REST_CHARACTER_REJECTED';
+export const CHARACTER_ACTION = 'CHARACTER_ACTION';
+export const CHARACTER_ACTION_PENDING = 'CHARACTER_ACTION_PENDING';
+export const CHARACTER_ACTION_FULFILLED = 'CHARACTER_ACTION_FULFILLED';
+export const CHARACTER_ACTION_REJECTED = 'CHARACTER_ACTION_REJECTED';
 
 const createClanAction = (clanName) => {
     return (dispatch) => {
@@ -76,7 +41,7 @@ export { createClanAction as createClan };
 const equipItemAction = (itemId, type, characterId) => {
     return (dispatch) => {
         return dispatch({
-            type: EQUIP_ITEM,
+            type: CHARACTER_ACTION,
             payload: equipItem(itemId, type, characterId)
         }).then(() => dispatch({
             type: FETCH_CLAN,
@@ -89,7 +54,7 @@ export { equipItemAction as equipItem };
 const unequipItemAction = (itemId, type, characterId) => {
     return (dispatch) => {
         return dispatch({
-            type: UNEQUIP_ITEM,
+            type: CHARACTER_ACTION,
             payload: unequipItem(itemId, type, characterId)
         }).then(() => dispatch({
             type: FETCH_CLAN,
@@ -102,7 +67,7 @@ export { unequipItemAction as unequipItem };
 const useConsumableAction = (consumableId, characterId) => {
     return (dispatch) => {
         return dispatch({
-            type: USE_CONSUMABLE,
+            type: CHARACTER_ACTION,
             payload: useConsumable(consumableId, characterId)
         }).then(() => dispatch({
             type: FETCH_CLAN,
@@ -115,7 +80,7 @@ export { useConsumableAction as useConsumable };
 const constructBuildingAction = (buildingName, characterId, type) => {
     return (dispatch) => {
         return dispatch({
-            type: BUILDING,
+            type: CHARACTER_ACTION,
             payload: constructBuilding(buildingName, characterId, type)
         }).then(() => dispatch({
             type: FETCH_CLAN,
@@ -128,7 +93,7 @@ export { constructBuildingAction as constructBuilding };
 const goOnQuestAction = (questId, characterId) => {
     return (dispatch) => {
         return dispatch({
-            type: QUEST,
+            type: CHARACTER_ACTION,
             payload: goOnQuest(questId, characterId)
         }).then(() => dispatch({
             type: FETCH_CLAN,
@@ -141,7 +106,7 @@ export { goOnQuestAction as goOnQuest };
 const tradeResourcesAction = (characterId, food, junk, type) => {
     return (dispatch) => {
         return dispatch({
-            type: TRADE_RESOURCES,
+            type: CHARACTER_ACTION,
             payload: tradeResources(characterId, food, junk, type)
         }).then(() => dispatch({
             type: FETCH_CLAN,
@@ -154,7 +119,7 @@ export { tradeResourcesAction as tradeResources };
 const visitLocationAction = (characterId, location, type) => {
     return (dispatch) => {
         return dispatch({
-            type: VISIT_LOCATION,
+            type: CHARACTER_ACTION,
             payload: visitLocation(characterId, location, type)
         }).then(() => dispatch({
             type: FETCH_CLAN,
@@ -167,7 +132,7 @@ export { visitLocationAction as visitLocation };
 const visitArenaAction = (characterId) => {
     return (dispatch) => {
         return dispatch({
-            type: VISIT_LOCATION,
+            type: CHARACTER_ACTION,
             payload: visitArena(characterId)
         }).then(() => dispatch({
             type: FETCH_CLAN,
@@ -180,7 +145,7 @@ export { visitArenaAction as visitArena };
 const visitTavernAction = (characterId) => {
     return (dispatch) => {
         return dispatch({
-            type: VISIT_LOCATION,
+            type: CHARACTER_ACTION,
             payload: visitTavern(characterId)
         }).then(() => dispatch({
             type: FETCH_CLAN,
@@ -193,7 +158,7 @@ export { visitTavernAction as visitTavern };
 const restWithCharacterAction = (characterId) => {
     return (dispatch) => {
         return dispatch({
-            type: REST_CHARACTER,
+            type: CHARACTER_ACTION,
             payload: restWithCharacter(characterId)
         }).then(() => dispatch({
             type: FETCH_CLAN,
--- src/reducers/actionReducer.js
+++ src/reducers/actionReducer.js
@@ -1,25 +1,7 @@
 import {
-    EQUIP_ITEM_FULFILLED,
-    EQUIP_ITEM_REJECTED,
-    EQUIP_ITEM_PENDING,
-    UNEQUIP_ITEM_FULFILLED,
-    UNEQUIP_ITEM_REJECTED,
-    UNEQUIP_ITEM_PENDING,
-    USE_CONSUMABLE_PENDING,
-    USE_CONSUMABLE_FULFILLED,
-    USE_CONSUMABLE_REJECTED,
-    BUILDING_PENDING,
-    BUILDING_FULFILLED,
-    BUILDING_REJECTED,
-    QUEST_PENDING,
-    QUEST_FULFILLED,
-    QUEST_REJECTED,
-    TRADE_RESOURCES_PENDING,
-    TRADE_RESOURCES_FULFILLED,
-    TRADE_RESOURCES_REJECTED,
-    VISIT_LOCATION_PENDING,
-    VISIT_LOCATION_FULFILLED,
-    VISIT_LOCATION_REJECTED
+    CHARACTER_ACTION_PENDING,
+    CHARACTER_ACTION_FULFILLED,
+    CHARACTER_ACTION_REJECTED
 } from '../actions/actionActions';
 
 import {
@@ -43,154 +25,21 @@ export const ActionReducer = (state = initialState, action) => {
                 failed: false,
                 error: ''
             };
-        case EQUIP_ITEM_PENDING:
+        case CHARACTER_ACTION_PENDING:
             return {
                 ...state,
                 fetching: true,
                 fetched: false,
                 failed: false
             };
-        case EQUIP_ITEM_FULFILLED:
+        case CHARACTER_ACTION_FULFILLED:
             return {
                 ...state,
                 fetching: false,
                 fetched: true,
                 failed: false
             };
-        case EQUIP_ITEM_REJECTED:
-            return {
-                ...state,
-                fetching: false,
-                fetched: true,
-                failed: true,
-                error: action.payload
-            };
-        case UNEQUIP_ITEM_PENDING:
-            return {
-                ...state,
-                fetching: true,
-                fetched: false,
-                failed: false
-            };
-        case UNEQUIP_ITEM_FULFILLED:
-            return {
-                ...state,
-                fetching: false,
-                fetched: true,
-                failed: false
-            };
-        case UNEQUIP_ITEM_REJECTED:
-            return {
-                ...state,
-                fetching: false,
-                fetched: true,
-                failed: true,
-                error: action.payload
-            };
-        case USE_CONSUMABLE_PENDING:
-            return {
-                ...state,
-                fetching: true,
-                fetched: false,
-                failed: false
-            };
-        case USE_CONSUMABLE_FULFILLED:
-            return {
-                ...state,
-                fetching: false,
-                fetched: true,
-                failed: false
-            };
-        case USE_CONSUMABLE_REJECTED:
-            return {
-                ...state,
-                fetching: false,
-                fetched: true,
-                failed: true,
-                error: action.payload
-            };
-        case BUILDING_PENDING:
-            return {
-                ...state,
-                fetching: true,
-                fetched: false,
-                failed: false
-            };
-        case BUILDING_FULFILLED:
-            return {
-                ...state,
-                fetching: false,
-                fetched: true,
-                failed: false
-            };
-        case BUILDING_REJECTED:
-            return {
-                ...state,
-                fetching: false,
-                fetched: true,
-                failed: true,
-                error: action.payload
-            };
-        case QUEST_PENDING:
-            return {
-                ...state,
-                fetching: true,
-                fetched: false,
-                failed: false
-            };
-        case QUEST_FULFILLED:
-            return {
-                ...state,
-                fetching: false,
-                fetched: true,
-                failed: false
-            };
-        case QUEST_REJECTED:
-            return {
-                ...state,
-                fetching: false,
-                fetched: true,
-                failed: true,
-                error: action.payload
-            };
-        case TRADE_RESOURCES_PENDING:
-            return {
-                ...state,
-                fetching: true,
-                fetched: false,
-                failed: false
-            };
-        case TRADE_RESOURCES_FULFILLED:
-            return {
-                ...state,
-                fetching: false,
-                fetched: true,
-                failed: false
-            };
-        case TRADE_RESOURCES_REJECTED:
-            return {
-                ...state,
-                fetching: false,
-                fetched: true,
-                failed: true,
-                error: action.payload
-            };
-        case VISIT_LOCATION_PENDING:
-            return {
-                ...state,
-                fetching: true,
-                fetched: false,
-                failed: false
-            };
-        case VISIT_LOCATION_FULFILLED:
-            return {
-                ...state,
-                fetching: false,
-                fetched: true,
-                failed: false,
-                error: ''
-            };
-        case VISIT_LOCATION_REJECTED:
+        case CHARACTER_ACTION_REJECTED:
             return {
                 ...state,
                 fetching: false,
