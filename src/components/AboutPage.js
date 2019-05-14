beda9b135bb64cb719fc2759acfecb68822c457a
Martin Myslik <martin.myslik@ataccama.com>
2019-05-14 15:56:21 +0200

    task/#56 Add thank you section to about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,15 +1,69 @@
 import React, { Component } from 'react';
 import { Translate } from "react-localize-redux";
+import { Table } from 'react-bootstrap';
 
 class AboutPage extends Component {
     render() {
         return (
             <div className="m-4">
                 <p>
-                    <Translate id="about.text" />
+                    <h3><Translate id="about.aboutHeader" /></h3>
+                    <Translate id="about.aboutText" />
                 </p>
                 <p>
-                    <Translate id="about.contribution" />
+                    <h3><Translate id="about.contributionHeader" /></h3>
+                    <Translate id="about.contributionText" />
+                </p>
+                <p>
+                    <h3><Translate id="about.ackHeader" /></h3>
+                    <Translate id="about.photosText" />
+                
+                    <Table className="mt-4" striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th><Translate id="basic.name" /></th>
+                                <th><Translate id="about.links" /></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Junktown crew</td>
+                                <td>
+                                    <a href="https://www.junktown.eu/">Web</a>, <a href="https://www.facebook.com/junktownfestival/">Facebook</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Petr Zip Hájek</td>
+                                <td>
+                                    <a href="http://hajek.photo">Web</a>, <a href="https://www.facebook.com/Petr.Zip.Hajek">Facebook</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Anna Baštýřová</td>
+                                <td>
+                                    <a href="https://www.facebook.com/bastyr.anna">Facebook</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Matej Slaviboj Bášti</td>
+                                <td>
+                                    <a href="https://www.facebook.com/matej.basti">Facebook</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>EXPit thru lens</td>
+                                <td>
+                                    <a href="https://www.facebook.com/EXPit/">Facebook</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Flying Sausages</td>
+                                <td>
+                                    <a href="https://www.facebook.com/flying.sausages/">Facebook</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </Table>
                 </p>
             </div>
         );
