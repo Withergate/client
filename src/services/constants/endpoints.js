da1224305105ab21b0a4497306487e3f0e79ab02
Martin Myslik <m.myslik@gmail.com>
2019-05-19 21:31:50 +0200

    bug/#87 Fix NPE when unequipping outfit

--- src/services/constants/endpoints.js
+++ src/services/constants/endpoints.js
@@ -1,15 +1,16 @@
 export const AUTH_URL = 'http://auth.withergate.com/';
-export const API_URL = 'http://api.withergate.com/';
-// export const API_URL = 'http://localhost:8080/';
-
-export const TOKEN_URL =
-  AUTH_URL +
-  'oauth/authorize' +
-  '?response_type=token&client_id=withergate&redirect_uri=http://game.withergate.com';
+// export const API_URL = 'http://api.withergate.com/';
+export const API_URL = 'http://localhost:8080/';
 
 /*
 export const TOKEN_URL =
   AUTH_URL +
   'oauth/authorize' +
-  '?response_type=token&client_id=withergate&redirect_uri=http://localhost:3000';
+  '?response_type=token&client_id=withergate&redirect_uri=http://game.withergate.com';
 */
+
+export const TOKEN_URL =
+  AUTH_URL +
+  'oauth/authorize' +
+  '?response_type=token&client_id=withergate&redirect_uri=http://localhost:3000';
+
