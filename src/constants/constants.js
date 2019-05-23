86993abefdc9d18305f0d132a7b11023ad9c61dd
Martin Myslik <m.myslik@gmail.com>
2019-05-23 14:56:41 +0200

    task/#75 Enable character selection in tavern

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -7,8 +7,12 @@ export const CONSUMABLE = 'CONSUMABLE';
 
 // Characters
 export const ROOKIE = 'ROOKIE';
+<<<<<<< HEAD
 export const VETERAN = 'VETERAN';
 
 // Default chanction
 export const REST = 'REST';
 export const EXPLORE_NEIGHBORHOOD = 'EXPLORE_NEIGHBORHOOD';
+=======
+export const VETERAN = 'VETERAN';
+>>>>>>> 6992332... task/#75 Enable character selection in tavern
