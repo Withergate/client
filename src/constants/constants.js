dbd1194be29cc0ccd0551c472158d4e2c1549471
Martin Myslik <m.myslik@gmail.com>
2019-05-26 11:32:37 +0200

    task/#83 Add support for default actions

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -8,11 +8,18 @@ export const CONSUMABLE = 'CONSUMABLE';
 // Characters
 export const ROOKIE = 'ROOKIE';
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 2b5cfd6... task/#83 Add support for default actions
 export const VETERAN = 'VETERAN';
 
 // Default chanction
 export const REST = 'REST';
+<<<<<<< HEAD
 export const EXPLORE_NEIGHBORHOOD = 'EXPLORE_NEIGHBORHOOD';
 =======
 export const VETERAN = 'VETERAN';
 >>>>>>> 6992332... task/#75 Enable character selection in tavern
+=======
+export const EXPLORE_NEIGHBORHOOD = 'EXPLORE_NEIGHBORHOOD';
+>>>>>>> 2b5cfd6... task/#83 Add support for default actions
