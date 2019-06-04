0f2e1b15ea9c97af471537cb47b89034bef0fb1f
Martin Myslik <martin.myslik@ataccama.com>
2019-06-04 10:35:43 +0200

    task/#86 Display client version in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { version as clientVersion }  from '../../package.json';
+
+
 const Footer = ({version}) => (
-    <div className="w-80 m-4"><small className="float-right text-muted">{version}</small></div>
+    <div className="w-80 m-4"><small className="float-right text-muted">api: {version} / client: {clientVersion}</small></div>
 );
 
 Footer.propTypes = {
