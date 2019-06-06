0d27bc3305e38807fcb77de83bbfb9abcb3f7126
Martin Myslik <martin.myslik@ataccama.com>
2019-06-06 17:21:02 +0200

    task/#87 Use env variable for redirect toggle

--- src/services/constants/endpoints.js
+++ src/services/constants/endpoints.js
@@ -2,14 +2,18 @@ export const AUTH_URL = 'http://auth.withergate.com/';
 export const API_URL = 'http://api.withergate.com/';
 // export const API_URL = 'http://localhost:8080/';
 
-export const TOKEN_URL =
-  AUTH_URL +
-  'oauth/authorize' +
-  '?response_type=token&client_id=withergate&redirect_uri=http://game.withergate.com';
+var tokenUrl;
+if (process.env.NODE_ENV !== 'production') {
+  tokenUrl =
+    AUTH_URL +
+    'oauth/authorize' +
+    '?response_type=token&client_id=withergate&redirect_uri=http://localhost:3000';
+} else {
+  tokenUrl =
+    AUTH_URL +
+    'oauth/authorize' +
+    '?response_type=token&client_id=withergate&redirect_uri=http://game.withergate.com';
+}
 
-/*
-export const TOKEN_URL =
-  AUTH_URL +
-  'oauth/authorize' +
-  '?response_type=token&client_id=withergate&redirect_uri=http://localhost:3000';
-*/
+export const TOKEN_URL = tokenUrl;
+
