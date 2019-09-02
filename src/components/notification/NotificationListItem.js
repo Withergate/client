db29858d0e81269981cca74cc1866198aa014572
Martin Myslik <martin.myslik@ataccama.com>
2019-09-02 10:54:28 +0200

    task/#97 Fix notification layout on mobile

--- src/components/notification/NotificationListItem.js
+++ src/components/notification/NotificationListItem.js
@@ -14,29 +14,31 @@ const renderDetail = detail => (
 const NotificationListItem = ({notification}) => (
     <Card className="m-4">
         <Card.Body>
-            <Card.Title>
-                { notification.header && <b>{notification.header}</b> }
-                <small className="float-right">
-                    <ul className="list-inline">
-                    { notification.foodIncome !== 0 && <li className="list-inline-item"><GameIcon type={FOOD} value={notification.foodIncome} size={LARGE} noPadding={true} /></li> }
-                    { notification.junkIncome !== 0 && <li className="list-inline-item"><GameIcon type={JUNK} value={notification.junkIncome} size={LARGE} noPadding={true} /></li> }
-                    { notification.capsIncome !== 0 && <li className="list-inline-item"><GameIcon type={CAPS} value={notification.capsIncome} size={LARGE} noPadding={true} /></li> }
-                    { notification.fameIncome !== 0 && <li className="list-inline-item"><GameIcon type={FAME} value={notification.fameIncome} size={LARGE} noPadding={true} /></li> }
-                    { notification.injury !== 0 && <li className="list-inline-item"><GameIcon type={INJURY} value={notification.injury} size={LARGE} noPadding={true} /></li> }
-                    { notification.healing !== 0 && <li className="list-inline-item"><GameIcon type={HEALING} value={notification.healing} size={LARGE} noPadding={true} /></li> }
-                    { notification.experience !== 0 && <li className="list-inline-item"><GameIcon type={EXPERIENCE} value={notification.experience} size={LARGE} noPadding={true} /></li> }
-                    { notification.information !== 0 && <li className="list-inline-item"><GameIcon type={INFORMATION} value={notification.information} size={LARGE} noPadding={true} /></li> }
-                    { notification.death && <li className="list-inline-item"><GameIcon type={DEATH} size={LARGE} noPadding={true} /></li> }
-                    </ul>
-                </small>
-            </Card.Title>            
+                <Row>
+                    <Col>
+                    { notification.header && <b>{notification.header}</b> }
+                    <small className="float-right">
+                        <ul className="list-inline">
+                        { notification.foodIncome !== 0 && <li className="list-inline-item"><GameIcon type={FOOD} value={notification.foodIncome} size={LARGE} noPadding={true} /></li> }
+                        { notification.junkIncome !== 0 && <li className="list-inline-item"><GameIcon type={JUNK} value={notification.junkIncome} size={LARGE} noPadding={true} /></li> }
+                        { notification.capsIncome !== 0 && <li className="list-inline-item"><GameIcon type={CAPS} value={notification.capsIncome} size={LARGE} noPadding={true} /></li> }
+                        { notification.fameIncome !== 0 && <li className="list-inline-item"><GameIcon type={FAME} value={notification.fameIncome} size={LARGE} noPadding={true} /></li> }
+                        { notification.injury !== 0 && <li className="list-inline-item"><GameIcon type={INJURY} value={notification.injury} size={LARGE} noPadding={true} /></li> }
+                        { notification.healing !== 0 && <li className="list-inline-item"><GameIcon type={HEALING} value={notification.healing} size={LARGE} noPadding={true} /></li> }
+                        { notification.experience !== 0 && <li className="list-inline-item"><GameIcon type={EXPERIENCE} value={notification.experience} size={LARGE} noPadding={true} /></li> }
+                        { notification.information !== 0 && <li className="list-inline-item"><GameIcon type={INFORMATION} value={notification.information} size={LARGE} noPadding={true} /></li> }
+                        { notification.death && <li className="list-inline-item"><GameIcon type={DEATH} size={LARGE} noPadding={true} /></li> }
+                        </ul>
+                    </small>
+                    </Col>
+                </Row>
             <Row>
-                    {   notification.imageUrl &&
-                        <Col md={1}>
-                         <Image align="left" width="60px" src={notification.imageUrl} />
-                        </Col>
-                    }
-                <Col>
+                { notification.imageUrl &&
+                    <Col md={1}>
+                        <Image width="60px" src={notification.imageUrl} />
+                    </Col>
+                }
+                <Col md={11}>
                     { getTranslatedText(notification.text) }
                     {
                         notification.details.length > 0 && 
