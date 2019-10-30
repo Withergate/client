63c440ec8be6d344e9d0eeae89f1e1a509a7ff2b
Martin Myslik <martin.myslik@ataccama.com>
2019-10-30 11:54:06 +0100

    task/#120 Open external links in new tab

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -15,20 +15,22 @@ class AboutPage extends Component {
                 <h3><Translate id="about.aboutHeader" /></h3>
                 <p><Translate id="about.aboutText" /></p>
 
+                <h3><Translate id="about.tutorialHeader" /></h3>
+                <p><Translate id="about.tutorialText" /></p>
                 <p>
-                    <Image src={CzechIcon} width="25px" className="mr-2" /> <a href="https://witherpedia.fandom.com/cs/wiki/N%C3%A1pov%C4%9Bda_ke_h%C5%99e">Wiki</a>
+                    <Image src={CzechIcon} width="25px" className="mr-2" /> <a href="https://witherpedia.fandom.com/cs/wiki/N%C3%A1pov%C4%9Bda_ke_h%C5%99e" target="_blank" rel="noopener noreferrer">Wiki</a>
                 </p>
 
                 <h3><Translate id="about.contributionHeader" /></h3>
                 <p><Translate id="about.contributionText" /></p>
 
-                <p><Image src={PatreonIcon} width="25px" className="mr-2" /> <b>Patreon: </b><a href="https://www.patreon.com/withergate">link</a></p>
+                <p><Image src={PatreonIcon} width="25px" className="mr-2" /> <b>Patreon: </b><a href="https://www.patreon.com/withergate" target="_blank" rel="noopener noreferrer">link</a></p>
                 <p><Image src={BitcoinIcon} width="25px" className="mr-2" /> <b>Bitcoin: </b>{ window._env_.BITCOIN }</p>
                 <p><Image src={CardanoIcon} width="25px" className="mr-2" /> <b>Cardano: </b>{ window._env_.CARDANO }</p>
                 
                 <h3><Translate id="about.developmentHeader" /></h3>
                 <p><Translate id="about.developmentText" /></p>
-                <p><Image src={GithubIcon} width="25px" className="mr-2" /> <a href="https://github.com/withergate">Github</a></p>
+                <p><Image src={GithubIcon} width="25px" className="mr-2" /> <a href="https://github.com/withergate" target="_blank" rel="noopener noreferrer">Github</a></p>
 
                 <h3><Translate id="about.ackHeader" /></h3>
                 <p><Translate id="about.photosText" /></p>
@@ -44,37 +46,37 @@ class AboutPage extends Component {
                         <tr>
                             <td>Junktown crew</td>
                             <td>
-                                <a href="https://www.junktown.eu/">Web</a>, <a href="https://www.facebook.com/junktownfestival/">Facebook</a>
+                                <a href="https://www.junktown.eu/" target="_blank" rel="noopener noreferrer">Web</a>, <a href="https://www.facebook.com/junktownfestival/" target="_blank" rel="noopener noreferrer">Facebook</a>
                             </td>
                         </tr>
                         <tr>
                             <td>Petr Zip Hájek</td>
                             <td>
-                                <a href="http://hajek.photo">Web</a>, <a href="https://www.facebook.com/Petr.Zip.Hajek">Facebook</a>
+                                <a href="http://hajek.photo" target="_blank" rel="noopener noreferrer">Web</a>, <a href="https://www.facebook.com/Petr.Zip.Hajek" target="_blank" rel="noopener noreferrer">Facebook</a>
                             </td>
                         </tr>
                         <tr>
                             <td>Anna Baštýřová</td>
                             <td>
-                                <a href="https://www.facebook.com/bastyr.anna">Facebook</a>
+                                <a href="https://www.facebook.com/bastyr.anna" target="_blank" rel="noopener noreferrer">Facebook</a>
                             </td>
                         </tr>
                         <tr>
                             <td>Matej Slaviboj Bášti</td>
                             <td>
-                                <a href="https://www.facebook.com/matej.basti">Facebook</a>
+                                <a href="https://www.facebook.com/matej.basti" target="_blank" rel="noopener noreferrer">Facebook</a>
                             </td>
                         </tr>
                         <tr>
                             <td>EXPit thru lens</td>
                             <td>
-                                <a href="https://www.facebook.com/EXPit/">Facebook</a>
+                                <a href="https://www.facebook.com/EXPit/" target="_blank" rel="noopener noreferrer">Facebook</a>
                             </td>
                         </tr>
                         <tr>
                             <td>Flying Sausages</td>
                             <td>
-                                <a href="https://www.facebook.com/flying.sausages/">Facebook</a>
+                                <a href="https://www.facebook.com/flying.sausages/" target="_blank" rel="noopener noreferrer">Facebook</a>
                             </td>
                         </tr>
                     </tbody>
