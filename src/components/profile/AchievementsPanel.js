bc837a6c480b17ee9f4da42b2657632600dd17a5
Martin Myslik <m.myslik@gmail.com>
2020-02-26 14:24:38 +0100

    task/#183 Display badge for new achievements and update react scripts

--- src/components/profile/AchievementsPanel.js
+++ src/components/profile/AchievementsPanel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Image, Row, Col } from 'react-bootstrap';
+import { Card, Image, Row, Col, Badge } from 'react-bootstrap';
 import { orderBy } from "lodash";
 import ReactTooltip from 'react-tooltip';
 
@@ -11,6 +11,14 @@ const sort = (list) => {
     return orderBy(list, ['date'], ['desc']);
 }
 
+const isToday = (someDate) => {
+    const date = new Date(someDate)
+    const today = new Date()
+    return date.getDate() === today.getDate()
+      && date.getMonth() === today.getMonth()
+      && date.getFullYear() === today.getFullYear()
+  }
+
 const AchievementsPanel = ({achievements}) => (
     <Card className="p-3 mb-4" border="none">
         <h5><Translate id="profile.achievementsUnlocked" /></h5>
@@ -19,7 +27,9 @@ const AchievementsPanel = ({achievements}) => (
             {
                 sort(achievements).map(achievement =>
                     <Col md={2} xs={4} key={achievement.id} data-tip data-for={achievement.details.identifier} >
-                        <Image src={achievement.details.imageUrl} width="80px" />
+                        <Image src={achievement.details.imageUrl} width="80px"></Image>
+                        { isToday(achievement.date) && <Badge variant="danger" className="corner-align"><Translate id="labels.new" /></Badge> }
+                        
                         <ReactTooltip id={achievement.details.identifier} effect="solid" className="tooltip-multiline">
                             <Row><Col><b>{getTranslatedText(achievement.details.name)}</b></Col></Row>
                             <Row><Col>{getTranslatedText(achievement.details.description)}</Col></Row>
